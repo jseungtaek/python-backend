1cd76bd6ed14599d6ee4ebb52ead221d6e78860c
전승택 <jeonseungtaek@jeonseungtaeg-ui-MacBookPro.local>
2022-10-19 17:01:26 +0900

    practice

---
+++ python/python-flask/app.py
@@ -0,0 +1,19 @@
+from email import message
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/<message>')
+def get_message(message):
+    return "<message> %s" % message
+
+if __name__ == '__main__':
+    app.run()
